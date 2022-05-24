bc5ef99cc81da169faf92e96d176cccfa8706e15
Christian Richter <christian@Christians-MacBook-Air.local>
2022-05-24 21:53:30 +0200

    added targets for building bix and iix files

--- raspi/gpio-tracer/firmware/fx2/extra_script.py
+++ raspi/gpio-tracer/firmware/fx2/extra_script.py
@@ -1,5 +1,26 @@
-Import("env")
+Import("env", "projenv")
+
+from os.path import join
+
+VID = int(env.GetProjectOption("VID"), 16)
+PID = int(env.GetProjectOption("PID"), 16)
+
+libdeps_dir = env.get("PROJECT_LIBDEPS_DIR")
+board = env.get("BOARD")
+fx2lib_utils = join(libdeps_dir, board, "fx2lib", "utils")
 
 # Addings this flags through build_flags causes the linker options to be appended to all compilation steps,
 # except for the final compilation of the firmware.hex.
 env.Append(LINKFLAGS=["-Wl-bDSCR_AREA=0x3e00",  "-Wl-bINT2JT=0x3f00"])
+
+env.AddCustomTarget(
+    "bix",
+    "$BUILD_DIR/${PROGNAME}.hex",
+    "$OBJCOPY -I ihex -O binary $BUILD_DIR/${PROGNAME}.hex $BUILD_DIR/${PROGNAME}.bix"
+)
+
+env.AddCustomTarget(
+    "iix",
+    "$BUILD_DIR/${PROGNAME}.hex",
+    "{} -v {} -p {} {} {}".format(join(fx2lib_utils, "ihx2iic.py"), VID, PID, "$BUILD_DIR/${PROGNAME}.hex", "$BUILD_DIR/${PROGNAME}.iic")
+)
