879146cd8e6bd7c1e00dddbb9ba74d7150476133
Christian Richter <christian@Christians-MacBook-Air.local>
2022-05-26 20:40:45 +0200

    delete superfluous files

--- raspi/gpio-tracer/rest.el
+++
@@ -1,9 +0,0 @@
-# -*- restclient -*-
-# For testing the rest server implementation
-#
-
-GET http://localhost:5000/start
-User-Agent: Emacs Restclient
-
-GET http://localhost:5000/state
-User-Agent: Emacs Restclient
